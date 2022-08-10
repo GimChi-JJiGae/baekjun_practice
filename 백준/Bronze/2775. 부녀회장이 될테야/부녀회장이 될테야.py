e36609495b6a77c745e05b0099b4d5d5a7e10c54
Dohyeon <54613889+GimChi-JJiGae@users.noreply.github.com>
2022-08-11 08:47:08 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+for i in range(n):
+    k = int(input())
+    n = int(input())
+    li = []
+    li2 = []
+    for j in range(1, n+1):
+        li.append(j)
+    for s in range(k):
+        for x in range(1,n+1):
+            li2.append(sum(li[:x]))
+        li = li2[:]
+        li2=[]
+    print(li[-1])
