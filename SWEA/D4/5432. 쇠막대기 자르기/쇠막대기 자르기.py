50908a005adc7e6f800c0f9039bc5bbf9b364085
Dohyeon <54613889+GimChi-JJiGae@users.noreply.github.com>
2022-08-19 10:30:52 +0900

    [D4] Title: 쇠막대기 자르기, Time: 274 ms, Memory: 85,944 KB -BaekjoonHub

--- SWEA/D4/5432. 쇠막대기 자르기/쇠막대기 자르기.py
+++ SWEA/D4/5432. 쇠막대기 자르기/쇠막대기 자르기.py
@@ -4,24 +4,22 @@ testcase = int(input())
 for tc in range(1, testcase + 1):
     blank_list = list(input())
 
-
-    for i in range(len(blank_list)):
-        if blank_list[i] == "(" and blank_list[i + 1] == ")":   # 레이저 위치를 찾는다
-            blank_list[i] = "r_o"                               # raser_open
-            blank_list[i + 1] = "r_c"                           # raser_close
-            
-
-    bar_num = blank_list.count("(")
-    result = bar_num
-    count = 0
+    result = 0                                      # 결과적으로 만들어지는 쇠막대 수
+    bar_count = 0                                   # 전체 쇠막대수를 카운트하기 위한 변수
+    count = 0                                       # 현재 위치에 올려져 있는쇠막대 수를 카운트
     for i in range(len(blank_list)):
         if blank_list[i] == "(":
-            count += 1
+            if blank_list[i + 1] == ")":            # 레이저 위치일 경우 지금까지의 count를 결과값에 더함, 레이저가 자른 쇠막대만큼 쇠막대수가 늘어나므로
+                result += count
+            else:
+                count += 1                          # 레이저가 아닐경우 쇠막대가 하나 더 올라온 것임
+                bar_count += 1
         elif blank_list[i] == ")":
-            count -= 1
-        elif blank_list[i] == "r_o":
-            result += count
-
+            if blank_list[i - 1] == "(":            # 레이저였을 경우 그냥 지나감
+                continue
+            else:
+                count -= 1                          # 올려져 있던 쇠막대를 지나간 것임
+    result += bar_count                             # 결과값 = 레이저가 자른 횟수 + 기존 쇠막대 수
     print(f"#{tc} {result}")
 
 
