e16bf4cc8445b22933cae8c65ce2f457e09e603a
Dohyeon <54613889+GimChi-JJiGae@users.noreply.github.com>
2022-08-19 11:55:05 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,683 ms, Memory: 261,696 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -9,13 +9,13 @@ for tc in range(1, testcase + 1):
     day = 0
     while(True):
 
-        max_val = max(money)
-        max_day = money.index(max_val)
+        max_val = max(money)                    # 가장 비쌀 때의 값
+        max_day = money.index(max_val)          # 가장 비싼 날
         for i in range(max_day):
-            result += max_val - money[i]
+            result += max_val - money[i]        # 0번 인덱스부터 가장 비싼 날까지 값의 차이를 모두 합함
 
-        money = money[max_day + 1:]
-        if len(money) == 0:
+        money = money[max_day + 1:]             # 가장 비싼날 앞부분은 모두 자름
+        if len(money) == 0:                     # 잘랐을 때 리스트 길이가 0이되면 종료
             break
 
     print(f"#{tc} {result}")
