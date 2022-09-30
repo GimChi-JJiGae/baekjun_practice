abbafc22b28c7ca4e3e945af97e63b5786c69999
Dohyeon <54613889+GimChi-JJiGae@users.noreply.github.com>
2022-09-30 11:25:53 +0900

    [D3] Title: N-Queen, Time: 204 ms, Memory: 62,476 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.py
+++ SWEA/D3/2806. N－Queen/N－Queen.py
@@ -1,18 +1,19 @@
 def Queens(floor ,checked, we_can, n):
     global total
-    if floor == n:
+    if floor == n:      # 끝까지 순열을 다 만들 수 있으면 +1 해준다
         total += 1
 
-    for i in range(len(we_can)):                    # 우리가 뽑을 수 있는 것중
+    for i in range(len(we_can)):                        # 우리가 뽑을 수 있는 것중
         cannot = False
-        for j in range(len(checked)):   # 이미 한 것의 대각선에 걸리면 안된다.
-            if checked[j] + (floor - j) == we_can[i] or checked[j] - (floor - j) == we_can[i]:
+        for j in range(len(checked)):                   # 이미 한 것의 대각선에 걸리면 안된다.
+            if checked[j] + (floor - j) == we_can[i] or checked[j] - (floor - j) == we_can[i] or we_can[i] == checked[j]:
                 cannot = True
                 break
         if cannot:
             continue
 
-        Queens(floor + 1, checked + [we_can[i]], we_can[:i] + we_can[i + 1:], n)
+        Queens(floor + 1, checked + [we_can[i]], we_can, n)
+    return
 
 testcase = int(input())
 
