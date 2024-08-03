35da9ac10da6a621382e7d1a1d27ef8ad3147439
Dohyeon <54613889+GimChi-JJiGae@users.noreply.github.com>
2024-08-04 00:06:46 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.py
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -1,18 +1,12 @@
-arr = []
-
-for _ in range(9):
-    arr.append(list(map(int, input().split())))
-# 값 하나씩 탐색하며 최댓값을 찾는다.
-max_value = 0
+max_val = 0
 row = 0
 col = 0
-
 for i in range(9):
-    for j in range(9):
-        if arr[i][j] >= max_value:
-            max_value = arr[i][j]
-            row = i+1
-            col = j+1
-
-print(max_value)
+    arr = list(map(int, input().split()))
+    for j in range(len(arr)):
+        if arr[j] >= max_val:
+            row = i + 1
+            col = j + 1
+            max_val = arr[j]
+print(max_val)
 print(row, col)
